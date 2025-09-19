7445f817ec2e2b278bd3ad628091b543327f8962
artykerkz <161413471+artykerkz@users.noreply.github.com>
2025-09-19 01:58:13 -0400

    Update DriverClass.java
    copied and pasted from eclipse because i dont have intellij

--- src/main/java/com/mycompany/csc229_211review_lab02hw/DriverClass.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/DriverClass.java
@@ -1,5 +1,7 @@
 package com.mycompany.csc229_211review_lab02hw;
 
+
+import java.util.Scanner;
 /**
  *
  * @author MoaathAlrajab
@@ -7,22 +9,34 @@ package com.mycompany.csc229_211review_lab02hw;
 public class DriverClass {
 	
 	public static void main(String[] args) {
-		// ToDo 5: Fix the error
-		
-		// ToDo 6: Fix the constructor of Student class
-		
-		// ToDo 7: Add a toString method for Student class
 		
 		Student std1= new Student("James", 20);
 		
-		// ToDo 8: Set the gpa of the student using the scanner and user
-		// 			input and then print the output.
+		
+		try (Scanner sc = new Scanner(System.in)) {
+		
+			System.out.print("Enter GPA for " + std1.getName() + " (0.0 - 4.0): ");
+			if (sc.hasNextDouble()) {
+				double gpa = sc.nextDouble();
+				try {
+					std1.setGpa(gpa);
+				} catch (IllegalArgumentException ex) {
+					System.out.println("Invalid GPA; " + ex.getMessage());
+				}
+			} else {
+				System.out.println("Invalid number entered; leaving GPA at default 0.0.");
+			}
+			
+			sc.nextLine();
+			
+			System.out.print("Enter address for " + std1.getName() + ": ");
+			std1.setAddress(sc.nextLine());
+		}
+	
 		
 		System.out.println(std1);
 		
-		// ToDo 9: add comments and explain your code
 		
-		// ToDo 10: submit using a pull request.
 	}
 
 }
