0c417dbcc3cee974f52b985f4660ca14a1d7cc33
artykerkz <161413471+artykerkz@users.noreply.github.com>
2025-09-19 01:59:14 -0400

    Update Student.java

--- src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
@@ -4,13 +4,53 @@ package com.mycompany.csc229_211review_lab02hw;
  *
  * @author MoaathAlrajab
  */
-public class Student {
-	// ToDo 1: Make this class a child of Person
+public class Student extends Person {
+	private String address;
+	private double gpa;
 	
-	// ToDo 2: Fix the resulting errors
 	
-	// ToDo 3: Add a field for GPA and create setter and getter
+
+public Student(String name, int age) {
+	super(name, (short) age);
+	this.address = "";
+	this.gpa = 0.0;
+}
+
+
+
+public Student(String name, short age, String address) {
+	super(name, age);
+	this.address = address; 
+	this.gpa = 0.0;
+}
+
+
+@Override 
+public String getAddress() {
+	return address;
+}
+
+
+@Override
+public void setAddress(String address) {
+	this.address = (address == null) ? "" : address.trim();
+}
+
+public void setGpa(double gpa) {
+	if (gpa < 0.0 || gpa > 4.0) {
+		throw new IllegalArgumentException("GPA must be between 0.0 4.0");
+	}
+	this.gpa = gpa;
+}
+
+
+@Override 
+public String toString() {
+	return "Student{name='" + getName()
+		+"', age=" + getAge()
+		+", address='" + getAddress()
+		+"', gpa=" + String.format("%.2g", gpa) + "}";
+}
 	
-	// ToDo 4: Add comments to your code
 
 }
